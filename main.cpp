9e54ea24166b3c24b5c7acb736048e94c7acceec
Kudos Beluga <69732000+Kudostoy0u@users.noreply.github.com>
2021-11-29 21:24:20 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Wed Sep 25 2019                                           */
+/*    Description:  Speedbot (Drivetrain, No Gyro)                            */
+/*                                                                            */
+/*    Name: Alpha team bot                                                    */
+/*    Date: 11/22/21                                                          */
+/*    Class:                                                                  */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    1, 2           
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  Brain.Screen.print("pls work uwu");
+  // Find the input handlers (bulk of the code) in robot-config.cpp
+  vexcodeInit();
+}
